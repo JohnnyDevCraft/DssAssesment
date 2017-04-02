2ac466f771150f395bbde5a8a61ad25c2d2c9fd0
Johnathon Smith <johnathon.smith@tecuity.com>
2017-04-02 00:18:19 -0600

    WIP

--- ToDoList/ToDoList/ToDoList.Web/Scripts/ngapp/categoriesController.js
+++ ToDoList/ToDoList/ToDoList.Web/Scripts/ngapp/categoriesController.js
@@ -3,7 +3,13 @@
 
     ngApplication.controller(ctrlName, function ($scope) {
 
-        $scope.categories = { id: '1', Name:'name', Description:'some MSDescription' }    
+        $scope.categories = { [{ id: 1, Name: 'name', Description: 'some MSDescription' }, { Id: 2, Name: 'Other', Description: 'Another Category' }]};
+
+        $scope.EditItem = function (id) {
+
+            for (var i = 0; i < $scope.categories.Length
+
+        }
     }
 
 
