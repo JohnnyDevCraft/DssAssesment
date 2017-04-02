e86e1226cf837efdb4686ef0c2144d7bf5e4466c
Johnathon Smith <johnathon.smith@tecuity.com>
2017-04-02 15:36:12 -0600

    WIP

--- ToDoList/ToDoList/ToDoList.Web/Scripts/ngapp/categoriesController.js
+++ ToDoList/ToDoList/ToDoList.Web/Scripts/ngapp/categoriesController.js
@@ -3,12 +3,40 @@
 
     ngApplication.controller(ctrlName, function ($scope) {
 
-        $scope.categories = { [{ id: 1, Name: 'name', Description: 'some MSDescription' }, { Id: 2, Name: 'Other', Description: 'Another Category' }]};
+        $scope.categories = { [{ id: 1, Name: 'name', Description: 'some Description' }, { Id: 2, Name: 'Other', Description: 'Another Category' }]};
+
+        $scope.itemEditing = { id: 1, Name: 'name', Description: 'Some Description' };
+
+        $scope.GetEditItem = function (id) {
+
+            for (var i = 0; i < $scope.categories.Length; i++) {
+                if ($scope.categories[i].Id == id) {
+                    $scope.itemEditing = $scope.categories[i];
+                }
+            }
+        }
+
+        $scope.view = "List";
+
+        $scope.itemCreating = {};
 
         $scope.EditItem = function (id) {
+            $scope.GetEditItem(id);
+            $scope.view = "Edit";
+        }
 
-            for (var i = 0; i < $scope.categories.Length
+        $scope.NewItem = function () {
+            $scope.itemCreating.Name = '';
+            $scope.itemCreating.Description = '';
+            $scope.view = "Create";
+        }
 
+        $scope.CancelEdit = function () {
+            $scope.view = "List";
+        }
+
+        $scope.CancelNew = function () {
+            $scope.view = "List";
         }
     }
 
